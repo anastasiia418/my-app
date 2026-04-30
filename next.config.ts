ad0ce8096c9509ca5b9ec925d3c8a7aef2e03db3
Anastasiia Zakala <anastasiazakala@gmail.com>
2026-04-30 13:07:09 -0700

    Deploy my-app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? '/my-app' : '',
+  basePath: isProd ? '/my-app' : '',
+  output: 'export',
 };
 
 export default nextConfig;
